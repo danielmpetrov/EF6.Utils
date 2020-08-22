e5cfdf83dc628d425671297ae19c4f6cbdab53c2
Daniel Petrov <daniel221569@gmail.com>
2020-08-22 11:29:07 +0200

    Add test project

--- EF6.Utils.Demo.Data/AppDbContext.cs
+++ EF6.Utils.Demo.Data/AppDbContext.cs
@@ -1,9 +1,18 @@
-﻿using System.Data.Entity;
+﻿using System.Data.Common;
+using System.Data.Entity;
 
 namespace EF6.Utils.Demo.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbConnection connection) : base(connection, true)
+        {
+        }
+
         public DbSet<Comment> Comments { get; set; }
     }
 }
---
+++ EF6.Utils.Tests/Tests.cs
@@ -0,0 +1,57 @@
+﻿using EF6.Utils.Demo.Data;
+using Effort;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace EF6.Utils.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private AppDbContext _context;
+
+        [SetUp]
+        public void Initialize()
+        {
+            var connection = DbConnectionFactory.CreateTransient();
+            _context = new AppDbContext(connection);
+        }
+
+        [Test]
+        public void SaveChangesTimestamped_WhenAddingOneNewEntity_ShouldSetTheSameCreatedAndUpdatedDateTimes()
+        {
+            // Arrange
+            var comment = new Comment { Content = "Test" };
+            _context.Comments.Add(comment);
+
+            // Act
+            _context.SaveChangesTimestamped();
+
+            // Assert
+            comment.CreatedOn.Should().NotBe(default);
+            comment.UpdatedOn.Should().NotBe(default);
+            comment.CreatedOn.Should().Be(comment.UpdatedOn);
+        }
+
+        [Test]
+        public void SaveChangesTimestamped_WhenAddingMultipleNewEntities_ShouldSetTheSameCreatedAndUpdatedDateTimes()
+        {
+            // Arrange
+            var comments = new List<Comment>
+            {
+                new Comment { Content = "Comment 1" },
+                new Comment { Content = "Comment 2" },
+            };
+
+            _context.Comments.AddRange(comments);
+
+            // Act
+            _context.SaveChangesTimestamped();
+
+            // Assert
+            comments[0].CreatedOn.Should().Be(comments[1].CreatedOn);
+            comments[0].UpdatedOn.Should().Be(comments[1].UpdatedOn);
+        }
+    }
+}
