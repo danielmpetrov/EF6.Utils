ac1a8b2aed19ce6578a66faf68f9928174aeafee
Daniel Petrov <daniel221569@gmail.com>
2020-08-24 22:49:07 +0200

    Convert data csproj to new format

--- EF6.Utils.Demo.Data/Migrations/Configuration.cs
+++ EF6.Utils.Demo.Data/Migrations/Configuration.cs
@@ -3,27 +3,11 @@ using System.Data.Entity.Migrations;
 namespace EF6.Utils.Demo.Data.Migrations
 {
 
-    internal sealed class Configuration : DbMigrationsConfiguration<EF6.Utils.Demo.Data.AppDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<AppDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-        }
-
-        protected override void Seed(EF6.Utils.Demo.Data.AppDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
